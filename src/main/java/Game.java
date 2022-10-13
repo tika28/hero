8e8906b51cf1a63673f9c777062085dfbbd91274
Rui Moreira <rui_scp_88@hotmail.com>
2022-10-13 18:17:08 +0100

    Reading Keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -10,6 +12,8 @@ import java.io.IOException;
 public class Game {
 
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game() {
         try {
@@ -27,11 +31,37 @@ public class Game {
 
     private void draw() throws IOException{
             screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
             screen.refresh();
     }
 
-    public void run() throws IOException{
-        draw();
+    private void processKey(KeyStroke key) throws IOException {
+        if (key.getKeyType() == KeyType.ArrowUp) {
+            y--;
+        }
+        if (key.getKeyType() == KeyType.ArrowDown) {
+            y++;
+        }
+        if (key.getKeyType() == KeyType.ArrowLeft) {
+            x--;
+        }
+        if (key.getKeyType() == KeyType.ArrowRight) {
+            x++;
+        }
+        if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+            screen.close();
+        }
     }
+    public void run() throws IOException{
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+            if(key.getKeyType() == KeyType.EOF){
+                break;
+            }
+        }
+
+    }
+
 }
