b31e5e851f6835d46eaec8ce4101f45da64c2b99
Rui Moreira <rui_scp_88@hotmail.com>
2022-10-13 17:11:46 +0100

    Game Class created

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+
+        Game game = new Game();
+
+        game.run();
+
+    }
+}
